5877adaa23b4d85227967179dcbf0a4e8e5701be
sgod112 <hackgodsk112@gmail.com>
2024-04-15 23:49:50 +0530

    modified

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,8 @@ router.get('/', function (req, res, next) {
 
   res.render('index', { isLoggedIn: req.session._id ? true : false });
 });
-router.post("add-tips", isVerified, async function (req, res) {
-  try {
-    const { tips } = req.body
-    const createdTip = await tipsModel.create({
-      tips,
-      uploder: req.session._id || "Someone"
-    })
-    res.status(200).json(createdTip)
-  } catch (error) {
-    res.status(500).send("Internal Server Error")
-  }
-})
-
-router.get('/delete-tip:tipId', isVerified, async function (req, res) {
-  try {
-    const deleted = tipsModel.findByIdAndDelete({ tipId: req.params.tipId })
-    if (!deleted) {
-      throw new Error(`Tip ${req.params.tipId} not found`)
-    }
-    res.status(200).json("deleted")
-  } catch (error) {
-    res.status(500).send(error.message)
-  }
-})
+router.get('/doctorspage', function (req, res, next) {
+  res.render('doctorsPage')
+});
 
 module.exports = router;
